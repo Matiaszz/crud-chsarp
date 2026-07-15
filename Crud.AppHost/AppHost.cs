385592807ceacaec315c17d523bf733647c3fc68
Allan Matias <alangiovanepaes@gmail.com>
2026-07-15 20:53:56 -0300

    initial

---
+++ Crud.AppHost/AppHost.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+
+
+var server = builder.AddProject<Projects.Crud_Server>("server")
+    .WithHttpHealthCheck("/health")
+    .WithExternalHttpEndpoints();
+
+var post = builder.AddPostgres("postgres");
+
+
+var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
+    .WithReference(server)
+    .WaitFor(server);
+
+server.PublishWithContainerFiles(webfrontend, "wwwroot");
+
+builder.Build().Run();
