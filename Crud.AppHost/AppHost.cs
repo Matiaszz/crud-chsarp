834651ae9678d2dc3c5b39f8af4f80173f3c8e9c
Allan Matias <alangiovanepaes@gmail.com>
2026-07-15 21:02:17 -0300

    teste

--- Crud.AppHost/AppHost.cs
+++ Crud.AppHost/AppHost.cs
@@ -8,8 +8,8 @@ var server = builder.AddProject<Projects.Crud_Server>("server")
     .WithHttpHealthCheck("/health")
     .WithExternalHttpEndpoints();
 
-var post = builder.AddPostgres("postgres");
 
+var post = builder.AddPostgres("postgres");
 
 var webfrontend = builder.AddViteApp("webfrontend", "../frontend")
     .WithReference(server)
